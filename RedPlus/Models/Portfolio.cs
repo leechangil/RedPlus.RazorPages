952d325d9c9aa2793d95f7ba90fd48453d4cc70f
leechangil <leechangil12@gmail.com>
2021-05-21 18:05:05 +0900

    17_04

---
+++ RedPlus/Models/Portfolio.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace RedPlus.Models
+{
+    /// <summary>
+    /// 모델 클래스:Model, ViewModel, Dto, Object, Entity, ...
+    /// </summary>
+    public class Portfolio
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        [JsonPropertyName("img")]
+        public string Image { get; set; }
+        public override string ToString()
+        {
+            return JsonSerializer.Serialize<Portfolio>(this);
+        }
+    }
+}
---
+++ RedPlus/Services/PortfolioServiceJsonFile.cs
@@ -0,0 +1,25 @@
+﻿using RedPlus.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace RedPlus.Services
+{
+    public class PortfolioServiceJsonFile
+    {
+        public IEnumerable<Portfolio> GetPortfolios() {
+            var jsonFileName = @"C:\Users\CL\Desktop\CL\.net\Razorpages\RedPlus.RazorPages\Razor\RedPlus.RazorPages\RedPlus\wwwroot\Portfolios\portfolios.json";
+
+            using (var jsonFileReader = File.OpenText(jsonFileName))
+            {
+                var options = new JsonSerializerOptions() { PropertyNameCaseInsensitive = true };
+                var portfolios = JsonSerializer.Deserialize<Portfolio[]>(jsonFileReader.ReadToEnd(), options);
+                return portfolios;
+            }
+        }
+
+    }
+}
