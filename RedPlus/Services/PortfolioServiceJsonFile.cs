129caf57ae72f50ba137a081b9aedf678a3413ac
창일 이 <leechangil12@gmail.com>
2021-05-21 22:45:43 +0900

    json file

--- RedPlus/Services/PortfolioServiceJsonFile.cs
+++ RedPlus/Services/PortfolioServiceJsonFile.cs
@@ -11,8 +11,8 @@ namespace RedPlus.Services
     public class PortfolioServiceJsonFile
     {
         public IEnumerable<Portfolio> GetPortfolios() {
-            var jsonFileName = @"C:\Users\CL\Desktop\CL\.net\Razorpages\RedPlus.RazorPages\Razor\RedPlus.RazorPages\RedPlus\wwwroot\Portfolios\portfolios.json";
-
+            //var jsonFileName = @"C:\Users\CL\Desktop\CL\.net\Razorpages\RedPlus.RazorPages\Razor\RedPlus.RazorPages\RedPlus\wwwroot\Portfolios\portfolios.json";
+            var jsonFileName = @"C:\Users\CL\Desktop\VisualAcademy\RedPlus.RazorPages\RedPlus\wwwroot\Portfolios\portfolios.json";
             using (var jsonFileReader = File.OpenText(jsonFileName))
             {
                 var options = new JsonSerializerOptions() { PropertyNameCaseInsensitive = true };
